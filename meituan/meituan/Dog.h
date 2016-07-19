c32016343ef1b831d4c4f4fbdae72d929ffee331
rangolee <848559256@qq.com>
2016-07-19 15:07:26 +0800

    添加了dog类

---
+++ meituan/meituan/Dog.h
@@ -0,0 +1,13 @@
+//
+//  Dog.h
+//  meituan
+//
+//  Created by Rango on 16/7/19.
+//  Copyright © 2016年 RangoLee. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Dog : NSObject
+
+@end
---
+++ meituan/meituan/Dog.m
@@ -0,0 +1,13 @@
+//
+//  Dog.m
+//  meituan
+//
+//  Created by Rango on 16/7/19.
+//  Copyright © 2016年 RangoLee. All rights reserved.
+//
+
+#import "Dog.h"
+
+@implementation Dog
+
+@end
